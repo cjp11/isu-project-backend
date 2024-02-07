32582e4981cee16a8728cd53c14fb20697ceb8d4
minkyung <m1027k@naver.com>
2024-02-07 19:56:55 +0900

    o Member entity 생성

--- src/main/java/miniHp/domain/Member.java
+++ src/main/java/miniHp/domain/Member.java
@@ -15,27 +15,19 @@ public class Member {
     private Long id;
     @Column
     private String member_id;
-
     @Column
     private String name;
-
     @Column
     private String password;
-
     @Column
     private String email;
     @Column
     private String register_type;
-
     @Column
     private Date birth_Date;
     @Column
     private String gender;
     @Column
     private String address;
-
-
-
-
 
 }
