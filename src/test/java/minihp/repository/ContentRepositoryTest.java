25bc8ab0c4177394e8a2d17a12bebbdd2f230eae
cjp11 <ysp214101@gmail.com>
2024-04-18 12:14:17 +0900

    @SpringBootTest에 class 명시

--- src/test/java/minihp/repository/ContentRepositoryTest.java
+++ src/test/java/minihp/repository/ContentRepositoryTest.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(classes = ContentRepositoryTest.class)
 @Transactional(readOnly = true)
 class ContentRepositoryTest {
 
